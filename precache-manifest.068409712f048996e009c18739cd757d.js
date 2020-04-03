739496a0b37d92a9935c73ead953722e2f510f65
sheen4n <sheen.an.goh@gmail.com>
2020-04-03 22:55:47 +0800

    Updates

---
+++ precache-manifest.068409712f048996e009c18739cd757d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78081e8bbd29ef346a4dd94f90f20058",
+    "url": "/react-vidly/index.html"
+  },
+  {
+    "revision": "50f9acb6414baa5259da",
+    "url": "/react-vidly/static/css/2.0e769a35.chunk.css"
+  },
+  {
+    "revision": "e06310baa332d3b0617b",
+    "url": "/react-vidly/static/css/main.a5bee8a7.chunk.css"
+  },
+  {
+    "revision": "50f9acb6414baa5259da",
+    "url": "/react-vidly/static/js/2.1ad25414.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/react-vidly/static/js/2.1ad25414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06310baa332d3b0617b",
+    "url": "/react-vidly/static/js/main.2bb4e6e1.chunk.js"
+  },
+  {
+    "revision": "be42e148fe5e83808ce4",
+    "url": "/react-vidly/static/js/runtime-main.18f27294.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
