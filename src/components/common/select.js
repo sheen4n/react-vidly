cc0c26bea4c22e81ae7819246cb3337f10dccd82
Goh Sheen An <sheen.an.goh@gmail.com>
2020-04-28 10:52:42 +0800

    updated select

--- src/components/common/select.js
+++ src/components/common/select.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Select = ({ name, value, dataSet, onChange }) => {
   return (
     <div className='form-group'>
-      <label htmlFor={name}>Example select</label>
+      <label htmlFor={name}>Select {name}</label>
       <select className='form-control' id={{ name }} name={name} value={value} onChange={onChange}>
         <option value=''>Please Select</option>
-        {dataSet.map(option => (
+        {dataSet.map((option) => (
           <option key={option._id} value={option._id}>
             {option.name}
           </option>
